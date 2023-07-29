fa6a1e3b9e7e786dfef569d8dc3a1b9ebc6e5837
MohamedTarekEsmail <62348966+MohamedTarekEsmail@users.noreply.github.com>
2023-07-29 17:36:31 +0300

    Add files via upload

---
+++ Interface/ReviewerController.cs
@@ -0,0 +1,105 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interface;
+using PokemonReviewApp.Model;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewerController : Controller
+    {
+        private readonly IReviewerRepository _reviewerRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewerController(IReviewerRepository reviewerRepository, IMapper mapper)
+        {
+            _reviewerRepository = reviewerRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(Reviewer))]
+        public IActionResult GetReviewers()
+        {
+            var reviewers = _mapper.Map<List<ReviewerDto>>(_reviewerRepository.GetReviewers());
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(reviewers);
+        }
+
+        [HttpGet("{reviewerId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Reviewer>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewer(int reviewerId)
+        {
+            if (_reviewerRepository.ReviewerExists(reviewerId))
+            {
+                return NotFound();
+            }
+            var reviewer = _mapper.Map<ReviewerDto>(_reviewerRepository.GetReviewer(reviewerId));
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return Ok(reviewer);
+        }
+
+        [HttpGet("{reviewerId}/reviews")]
+        [ProducesResponseType (200, Type = typeof(Reviewer))]
+        [ProducesResponseType (400)]
+
+        public IActionResult GetReviewsbyAReviewer(int reviewerId)
+        {
+            if (!_reviewerRepository.ReviewerExists(reviewerId))
+            {
+                return NotFound();
+            }
+            var reviews = _mapper.Map<List<ReviewDto>>(_reviewerRepository.GetReviewsbyAReviewer(reviewerId));
+            if (!ModelState.IsValid)
+                return BadRequest();
+            return Ok(reviews);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateReview([FromBody] ReviewerDto reviewerCreate)
+        {
+            if (reviewerCreate == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var reviewers = _reviewerRepository.GetReviewers()
+                .Where(r => r.LastName.Trim().ToUpper()== reviewerCreate.LastName.TrimEnd().ToUpper())
+                .FirstOrDefault();
+
+            if (reviewers != null)
+            {
+                ModelState.AddModelError("", "reviewer already Exists");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var reviewerMap = _mapper.Map<Reviewer>(reviewerCreate);
+
+            if (!_reviewerRepository.CreateReviewer(reviewerMap))
+            {
+                ModelState.AddModelError("", "something wrong while saving");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("successfully saved");
+
+        }
+    }
+}
