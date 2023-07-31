125a0d1f85d4162e363ddcf6a565af8ad69b3f5f
MohamedTarekEsmail <62348966+MohamedTarekEsmail@users.noreply.github.com>
2023-07-31 20:29:24 +0300

    AddedDelete

--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -13,6 +13,38 @@ namespace PokemonReviewApp.Repository
            _context = context;
         }
 
+        public bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon)
+        {
+            var pokeOwnerEntity = _context.Owners.Where(a => a.Id == ownerId).FirstOrDefault();
+            var pokeCategoryEntity = _context.Categories.Where(a => a.ID == categoryId).FirstOrDefault();
+
+            var PokemonOwner = new PokemonOwner()
+                {
+                    Owner = pokeOwnerEntity,
+                    Pokemon = pokemon,
+                };
+            
+            _context.Add(PokemonOwner);
+
+            var pokemonCategory = new PokemonCategory()
+            {
+                Category = pokeCategoryEntity,
+                Pokemon = pokemon,
+            };
+
+            _context.Add(pokemonCategory);
+
+            _context.Add(pokemon);
+
+            return Save();
+        }
+
+        public bool DeletePokemon(Pokemon pokemon)
+        {
+            _context.Remove(pokemon);
+            return Save();
+        }
+
         public Pokemon GetPokemon(int id)
         {
             return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
@@ -40,6 +72,18 @@ namespace PokemonReviewApp.Repository
         public bool PokemonExists(int PokeId)
         {
             return _context.Pokemons.Any(p => p.Id == PokeId);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges() ;
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdatePokemon(Pokemon pokemon)
+        {
+            _context.Update(pokemon);
+            return Save();
         }
     }
 }
